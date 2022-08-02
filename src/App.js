0713732ae311cb0c4e98af34442a627615209ca2
Ezra-code <njerukithinji872@gmail.com>
2022-08-02 21:49:05 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Body from "./body";
+import Footer from "./footer"
+function App() {
+    return (
+        <div>
+            <Body />
+            <Footer />
+        </div>
+    )
+}
+export default App
