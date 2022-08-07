17350c6164f496db1a2ad2d0d7e09363af604175
Christopher chinia <chinia214@gmail.com>
2022-08-07 19:29:59 +0300

    merged body into home components and solved the merging conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
-import Body from "./body";
-import Footer from "./footer"
+import {Route, Switch} from "react-router-dom"
+import Header from "./components/Header"
+import Body from "./components/body";
+import Footer from "./components/footer"
 function App() {
     return (
-        <div>
-            <Body />
+        <div className="App">
+          <Header />
+          <Switch> 
+          <Route exact path="/home">
+          <Body />
             <Footer />
+        </Route> 
+            
+            </Switch>
         </div>
     )
 }
-export default App
-=======
-import React from 'react';
-import Header from './components/Header';
-
-function App() {
-  return (
-    <div className="App">
-  <Header />
-    </div>
-  );
-}
-
 export default App;
+
